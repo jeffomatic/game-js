b0a7a9ab7d00bc2516e37958f8094276fe0181c9
Jeff Lee <jeffomatic@gmail.com>
2019-02-18 15:57:04 -0800

    stl2mesh: combine parsing into single module

--- src/tools/stl2mesh/index.ts
+++ src/tools/stl2mesh/index.ts
@@ -1,9 +1,7 @@
 import * as fs from 'fs';
 import * as process from 'process';
-import * as _ from 'lodash';
 
-import { isAscii, parseAscii } from './ascii';
-import { parseBinary } from './binary';
+import { parse } from './parse_stl';
 import { convert } from './convert';
 
 if (process.argv.length !== 3) {
@@ -16,7 +14,7 @@ if (path === '-') {
 }
 
 const stl = fs.readFileSync(path);
-const triangles = isAscii(stl) ? parseAscii(stl) : parseBinary(stl);
+const triangles = parse(stl);
 const mesh = convert(triangles, { scale: 0.2 });
 
 console.log(JSON.stringify(mesh));
--- src/tools/stl2mesh/parse_stl.ts
+++ src/tools/stl2mesh/parse_stl.ts
@@ -1,4 +1,5 @@
 import * as _ from 'lodash'; // tslint:disable-line
+import * as binaryParser from 'binary-parser';
 
 enum State {
   Start,
@@ -127,4 +128,23 @@ export function parseAscii(buf: Buffer): number[][] {
     });
 
   return triangles;
+}
+
+export function parseBinary(buf: Buffer): number[][] {
+  const parser = new binaryParser.Parser()
+    .array('header', { type: 'uint8', length: 80 })
+    .uint32le('size')
+    .array('triangles', {
+      type: new binaryParser.Parser()
+        .array('normal', { type: 'floatle', length: 3 })
+        .array('vertices', { type: 'floatle', length: 9 })
+        .uint16le('attrib'),
+      length: 'size',
+    });
+
+  return parser.parse(buf).triangles.map(tri => tri.vertices);
+}
+
+export function parse(buf: Buffer): number[][] {
+  return isAscii(buf) ? parseAscii(buf) : parseBinary(buf);
 }
