77f714c0b5d7c267b8a477467dd1c5c4239b34c1
Jeff Lee <jeffomatic@gmail.com>
2019-02-21 00:10:21 -0800

    stl2mesh: fix convert test

--- src/tools/stl2mesh/__tests__/convert.test.ts
+++ src/tools/stl2mesh/__tests__/convert.test.ts
@@ -10,19 +10,19 @@ test('convert', () => {
   expect(convert([[-1, -1, -1, 0, 0, 0, 1, 1, 1]])).toEqual({
     vertices: [-1, -1, -1, 0, 0, 0, 1, 1, 1],
     triangleIndices: [0, 1, 2],
-    lineIndices: [0, 1, 1, 2, 2, 0],
+    lineIndices: [0, 1, 0, 2, 1, 2],
   });
 
   expect(convert([[0, 0, 0, 1, 1, 1, 2, 2, 2]])).toEqual({
     vertices: [-1, -1, -1, 0, 0, 0, 1, 1, 1],
     triangleIndices: [0, 1, 2],
-    lineIndices: [0, 1, 1, 2, 2, 0],
+    lineIndices: [0, 1, 0, 2, 1, 2],
   });
 
   expect(convert([[0, 0, 0, 1, 1, 1, 2, 2, 2]])).toEqual({
     vertices: [-1, -1, -1, 0, 0, 0, 1, 1, 1],
     triangleIndices: [0, 1, 2],
-    lineIndices: [0, 1, 1, 2, 2, 0],
+    lineIndices: [0, 1, 0, 2, 1, 2],
   });
 
   expect(
@@ -30,6 +30,6 @@ test('convert', () => {
   ).toEqual({
     vertices: [-1, -1, -1, 0, 0, 0, 1, 1, 1],
     triangleIndices: [0, 1, 2, 2, 0, 1],
-    lineIndices: [0, 1, 1, 2, 2, 0, 2, 0, 0, 1, 1, 2],
+    lineIndices: [0, 1, 0, 2, 1, 2],
   });
 });
